db6f11dc73b54864c47425d7cc2f2ccec8ea3fc3
Jake Nelson <j.n@live.com.au>
2019-10-01 20:55:11 +0800

    Initial Commit

---
+++ testcloudkit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testcloudkit
+//
+//  Created by Jake Nelson on 1/10/19.
+//  Copyright © 2019 Jake Nelson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
