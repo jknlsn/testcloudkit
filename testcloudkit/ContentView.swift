e40e48e65dd1c0205a791338a4d1e644fe44801a
Jake Nelson <j.n@live.com.au>
2019-10-02 17:36:38 +0800

    Printing in the console shows correct relationship

--- testcloudkit/ContentView.swift
+++ testcloudkit/ContentView.swift
@@ -7,39 +7,73 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) var managedObjectContext
     
     @State var totalClicked: Int = 0
+    @State var debugString: String = ""
+    
+    @FetchRequest(fetchRequest: fetchRequest(), animation: nil) var people : FetchedResults
+    
+    //Core Data Fetch Request
+    static func fetchRequest() -> NSFetchRequest<User> {
+        let request : NSFetchRequest = User.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "lastName", ascending: false)]
+        return request
+    }
+    
+    // Account fetch
+    @FetchRequest(fetchRequest: accountsFetchRequest(), animation: nil) var accounts : FetchedResults
+    
+    //Core Data Fetch Request
+    static func accountsFetchRequest() -> NSFetchRequest<Account> {
+        let request : NSFetchRequest = Account.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false)]
+        return request
+    }
     
     var body: some View {
         VStack {
             Text("\(totalClicked)").font(.title)
+            Text("\(debugString)").font(.title)
+            Spacer()
+            List (people, id: \.objectID) {person in
+                Text("\(person.lastName ?? "")")
+                Text("\(person.firstName ?? "")")
+            }
+            Spacer()
+            List (accounts, id: \.objectID) {account in
+                Text("no bueno")
+            }
             Spacer()
             Button(action: {
                 
                 self.totalClicked = self.totalClicked + 1
                 
-                /// Create Account
-                let account = Account(context: self.managedObjectContext)
+//                /// Create Account
+//                let account = Account(context: self.managedObjectContext)
+//                account.title = "Admin"
+//
+//                // Create User
+//                let user = User(context: self.managedObjectContext)
+//
+//                // Configure User
+//                user.firstName = "Bart"
+//                user.lastName = "Jacobs"
+//
+//                account.addToUsers(user)
+//                // account.users = user
+//
+//                do {
+//                    try self.managedObjectContext.save()
+//                } catch {
+//                    print(error)
+//                }
                 
-                // Create User
-                let user = User(context: self.managedObjectContext)
-                
-                // Configure User
-                user.firstName = "Bart"
-                user.lastName = "Jacobs"
-                
-                account.addToUsers(user)
-                // account.users = user
-                
-                do {
-                    try self.managedObjectContext.save()
-                } catch {
-                    print(error)
-                }
+                print(self.accounts[0].users?.allObjects)
                 
             }) {
                 Text("Increment Total")
@@ -47,6 +81,8 @@ struct ContentView: View {
         }.padding(.all, 40)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
